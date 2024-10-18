0f4e64ed80b950c68400795604c33f590c61b8ac
Dohány Botond <bdohany@gmail.com>
2024-10-18 14:53:40 +0200

    Initial commit

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -4,7 +4,7 @@ from main import Calculator
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
-        self.asserEqual(calc.add(2,3),5)
+        self.assertEqual(calc.add(2,3),5)
 
     def test_subtract(self):
         calc = Calculator()
